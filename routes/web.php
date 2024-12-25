d1f8f64114ef65d0526f3d7437ce88859584f6bb
Flávio Teixeira <teixeira.fla@gmail.com>
2024-12-25 18:45:50 -0300

    Criação das Tabelas
    Criação das tabelas da Base de Dados com o Laravel.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $helloworld = 'Hello World';
+    return view('welcome', ['helloworld' => $helloworld]);
 });
